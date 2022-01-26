b477dbe7aa24b3684e3aefcd71b623c33ab610ef
kunalsingla046 <97744640+kunalsingla046@users.noreply.github.com>
2022-01-26 17:47:53 +0530

    Add files via upload

---
+++ assignment 2/assignment 2(output).py
@@ -0,0 +1,55 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========= RESTART: C:\Users\Kunal Singla\Desktop\assignment2 (input).py ========
+ques 1
+a part
+length of given string is 35
+b part
+reversed string order is egaugnal evitisnes esac a si nohtyP
+c part
+new string is case sensitive
+d part
+the part of string replaced is Python is a object oriented language
+e part
+index of substring  is 10
+f part
+string without space is Pythonisacasesensitivelanguage
+
+
+ques 2
+Hey,kunal singla here!
+my sid is 21104046
+I am from EE department and my cgpa is 9.8 
+
+
+ques 3
+a part:
+a & b = 8
+b part:
+a | b = 58
+c part:
+a ^ b = 50
+d part:
+Left shifting a and b with 2 bits: a = 224 b = 40
+e part:
+Right shifting a with 2 bits and b with 4 bits: a = 224 b = 160
+
+
+ques 4
+enter the first number15
+enter the second number40
+enter the third number25
+The greatest number is 40
+
+
+ques 5
+enter the desired string: My name is kunal
+yes
+
+
+ques6
+Enter the length of first side:3
+Enter the length of second side:4
+Enter the length third side:5
+yes
---
+++ assignment 2/assignment2 (input).py
@@ -0,0 +1,93 @@
+print("ques 1")
+given_string ='Python is a case sensitive language'
+print("a part")
+print("length of given string is %s" % len(given_string))
+print("b part")
+print("reversed string order is %s" % given_string[::-1])
+print("c part")
+New_String = given_string[12:26]
+print("new string is %s" % New_String)
+print("d part")
+string_replaced = given_string.replace("case sensitive", "object oriented")
+print("the part of string replaced is %s" % string_replaced)
+print("e part")
+substring = "a"
+# finding "a" when it first occurred in substring
+print("index of substring  is %s" % given_string.find(substring))
+print("f part")
+print("string without space is %s" % given_string.replace(" ",""))
+
+print()
+print()
+
+print("ques 2")
+name = 'kunal singla'
+sid = 21104046
+department_name = 'EE'
+cgpa=9.8
+
+print("Hey,%s here!"%name)
+print("my sid is %s"%sid)
+print("I am from %s department and my cgpa is %s " %(department_name,cgpa))
+
+print()
+print()
+
+print("ques 3")
+a=56
+b=10
+print("a part:")
+print("a & b = %s" % (a&b))
+print("b part:")
+print("a | b = %s"%(a|b))
+print("c part:")
+print("a ^ b = %s"%(a^b))
+print("d part:")
+print("Left shifting a and b with 2 bits: a = %s b = %s"%(a<<2,b<<2))
+print("e part:")
+print("Right shifting a with 2 bits and b with 4 bits: a = %s b = %s"%(a<<2,b<<4))
+
+print()
+print()
+
+print("ques 4")
+# let a, b ,c be the three number entered by user
+a = input("enter the first number")
+b = input("enter the second number")
+c = input("enter the third number")
+if (a==b==c) :
+    print("The greatest number is %s" % a)
+elif (a>=b) :
+    if a>c :
+        print("The greatest number %s"% a)
+    else :
+        print("The greatest number is %s"% c)
+elif (b>=c) :
+    if (b>c) :
+        print("The greatest number is %s" % b)
+    else :
+        print("The greatest number is %s" % c)
+
+print()
+print()
+
+print("ques 5")
+String = input("enter the desired string:")
+if "name" in String:
+    print("yes")
+else:
+    print("no")
+
+print()
+print()
+
+print("ques6")
+# let a,b,c be the length of sides of triangle
+
+a = int(input("Enter the length of first side:"))
+b = int(input("Enter the length of second side:"))
+c = int(input("Enter the length third side:"))
+if a+b>c and b+c>a and a+c>b:
+    print("yes")
+else:
+    print("no")
