13e13cd741d4d25b875591ffad4e672d6746d718
kunalsingla046 <97744640+kunalsingla046@users.noreply.github.com>
2022-02-14 20:48:32 +0530

    Add files via upload
    Kunal Singla
    21104046
    EE

---
+++ assignment3/assignment 3 (output).py
@@ -0,0 +1,87 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======== RESTART: C:\Users\Kunal Singla\Desktop\assignment 3 (input).py ========
+ques 1
+Enter The desired string:KUNAL
+{'K': 1, 'U': 1, 'N': 1, 'A': 1, 'L': 1}
+
+
+ques 2
+Please Enter The Day: 12
+Please Enter The Month: 5
+Please Enter The Year: 2021
+Next date is 13/5/2021
+
+
+ques 3
+Enter a list of numbers: 2 4 6
+[(2, 4), (4, 16), (6, 36)]
+
+
+ques 4
+ enter your grade point:9
+your grade is A and Excellent performance 
+
+
+ques 5
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+
+ques 6
+enter your name:KUNAL
+Enter your sid:21104046
+Do you want other student details(Y or N)Y
+enter your name:YUG
+Enter your sid:21103085
+Do you want other student details(Y or N)Y
+enter your name:AAYUSH
+Enter your sid:21105029
+Do you want other student details(Y or N)N
+a part
+details stored in dictionary are {21104046: 'KUNAL', 21103085: 'YUG', 21105029: 'AAYUSH'}
+
+b part 
+the sorted dictionary is {21105029: 'AAYUSH', 21104046: 'KUNAL', 21103085: 'YUG'}
+
+c part
+sorting done according to{21103085: 'YUG', 21104046: 'KUNAL', 21105029: 'AAYUSH'}
+
+d part
+Enter the student sid to search21104046
+the students details you want to search is KUNAL
+
+
+ques 7
+enter the numberof terms of fibonacci series:12
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+55
+89
+the average of the series is 19.333333333333332
+
+
+ques 8
+a part.
+{1, 3, 5, 6, 8}
+b part.
+{3, 6, 8, 9, 13, 17}
+c part.
+{1, 2, 4, 5}
+d part.
+{6, 7, 8, 9, 10}
+e part.
+{10, 7}
